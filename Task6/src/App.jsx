ba106996f4addc09b1f71f01b7abd0483821a8ff
Bhagirathsinh8 <bhagirathnakum8@gmail.com>
2025-12-24 11:32:32 +0530

    Task-6
    Nested Route and 404 Page
    / - home
    /blog - nested route
    /* -404

--- Task6/src/App.jsx
+++ Task6/src/App.jsx
@@ -3,16 +3,32 @@ import './App.css'
 import BlogList from './componets/BlogList'
 import BlogDetails from './page/BlogDetails'
 import NotFound404 from './page/NotFound404'
+import Home from './page/Home'
 
 const browserRouter = createBrowserRouter([
   {
     path:'/',
-    element:<BlogList/>,
+    // element:<Home/>,
+    errorElement:<NotFound404/>,
+     children: [
+    {
+      index: true,      
+      element: <Home />,
+    },
+    {
+      path: 'blog',     
+      element: <BlogList />,
+    },
+  ],
   },
   {
     path:'/blog/:id',
-    element:<BlogDetails/>
+    element:<BlogDetails/>,
+    //use For this route if blog not found
+    errorElement:<NotFound404/>
   },
+  
+  //this use for any route not defined
   {
     path:"*",
     element:<NotFound404/>
